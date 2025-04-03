acd5a91b6db15e9923a4cb1da0e5b88595baa3af
maheshraut07 <rautmahesh213@gmail.com>
2025-04-03 17:16:05 +0530

    updated the recurrsion subsequences problem

--- 41DSA_Recurrsion_Subsets_using_recurrsion.cpp
+++ 41DSA_Recurrsion_Subsets_using_recurrsion.cpp
@@ -1,41 +1,44 @@
-//subsets using recurrsion
+#include <iostream>
+#include <vector>
+using namespace std;
 
- #include<iostream>
- #include<vector>
- using namespace std;
-
- void solve( vector<int> nums , vector<int> output , int index , vector<vector<int> > &ans){
-      if(index >= nums.size())
-      {
+void solve(vector<int>& nums, vector<int>& output, int index, vector<vector<int>>& ans) {
+    if (index >= nums.size()) {
         ans.push_back(output);
         return;
-      }
+    }
 
-      solve(nums, output, index+1, ans) ; // exclude
+    // Exclude the current element
+    solve(nums, output, index + 1, ans);
 
-      int element = nums[index];
-      output.push_back(element);
-      solve(nums, output, index+1, ans);
- }
+    // Include the current element
+    output.push_back(nums[index]);
+    solve(nums, output, index + 1, ans);
 
+    // Backtrack to remove the added element
+    output.pop_back();
+}
 
- vector<vector<int>>  subsets(vector<int>&nums){
+vector<vector<int>> subsets(vector<int>& nums) {
     vector<vector<int>> ans;
     vector<int> output;
-    int index = 0;
-    
-    solve(nums, output, index, ans);
+    solve(nums, output, 0, ans);
     return ans;
- }
+}
 
-
-
- int main(){
-
-    vector<int> nums = {1,2,3};
-
+int main() {
+    vector<int> nums = {1, 2, 3};
 
     vector<vector<int>> s = subsets(nums);
     
+    cout << "Subsets:" << endl;
+    for (const auto& subset : s) {
+        cout << "{ ";
+        for (int num : subset) {
+            cout << num << " ";
+        }
+        cout << "}" << endl;
+    }
+
     return 0;
- }
+}
